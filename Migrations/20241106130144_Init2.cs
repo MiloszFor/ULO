b5cf3090afef3c4e5fb08a97844bf223f98f8b45
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    0.01Dodaj pliki projektów.

---
+++ Migrations/20241106130144_Init2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ULO.UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aktualnosc",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aktualnosc", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aktualnosc");
+        }
+    }
+}
---
+++ Models/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Login { get; set; }
+        public required string Email { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Password { get; set; }
+        public required string Rank { get; set; }
+    }
+}
