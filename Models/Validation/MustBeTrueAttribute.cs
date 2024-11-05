0af434c27c0c9fd38b1d578a59db38001a34e6d5
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    dv3Defaultv3

---
+++ Models/Validation/MustBeTrueAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Validation
+{
+    public class MustBeTrueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            return value is bool boolValue && boolValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+using ULO;
+using ULO.UI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+.AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
+builder.Services.AddAntiforgery();
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "auth_token";
+        options.LoginPath = "/LoginPage";
+        options.Cookie.MaxAge = TimeSpan.FromMinutes(366);
+        options.AccessDeniedPath = "/Error";
+    });
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddDbContext<ULODbContext>
+                (options => options.UseSqlServer(builder.Configuration.GetConnectionString("ULODbConnection")));
+
+builder.Services.AddAuthorizationCore();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
