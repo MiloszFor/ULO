09f1d6c983bc79a14b36f4ab942968466081bb71
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    Umawianie Meczy

---
+++ Models/Entities/Match.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Entities
+{
+    public class Match
+    {
+        [Key]
+        public int Id { get; set; }
+        public int HostsTeamId { get; set; }
+        public int guestsTeamId { get; set; }
+        public string? Place { get; set; }
+        public DateTime? MatchDate { get; set; }
+        public char? Status { get; set; }
+    }
+}
---
+++ Models/RegisterTeam.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ULO.UI.Models.Validation;
+
+namespace ULO.UI.Models
+{
+    public class RegisterTeam
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string? TeamName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string? InstaUrl { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string? Address { get; set; }
+        [MustBeTrue(ErrorMessage = "Musisz zaakceptować regulamin.")]
+        public bool AcceptTerms { get; set; }
+        [Required(AllowEmptyStrings = true)]
+        public byte[]? Logo { get; set; }
+    }
+}
