7cbf68d3e78af4f2cb4c96a576b3d220ddff0b0c
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    0.02

---
+++ Migrations/20241107103222_Init3.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ULO.UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Logo = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Team");
+        }
+    }
+}
---
+++ Models/Entities/Team.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Entities
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+
+        public string TeamName { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false)]
+
+        public byte[] Logo { get; set; } = Array.Empty<byte>(); // Przechowywanie obrazu jako tablica bajtów
+        [Required(AllowEmptyStrings = false)]
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
