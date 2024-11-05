4169b22511a3236f46a626fb1bfe63434f6f7b4e
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    umawianieMeczyv2Umawianie Meczy

---
+++ Migrations/20241116033238_Init10.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ULO.UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "guestsTeamId",
+                table: "Match",
+                newName: "HostTeamId");
+
+            migrationBuilder.RenameColumn(
+                name: "HostsTeamId",
+                table: "Match",
+                newName: "GuestTeamId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HostTeamId",
+                table: "Match",
+                newName: "guestsTeamId");
+
+            migrationBuilder.RenameColumn(
+                name: "GuestTeamId",
+                table: "Match",
+                newName: "HostsTeamId");
+        }
+    }
+}
---
+++ Models/Entities/TeamPoz.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Entities
+{
+    public class TeamPoz
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? UserId { get; set; }
+        public int TeamId { get; set; }
+        public string? Position { get; set; }
+        public int Number { get; set; }
+        public char Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
