1254be323bbf4169cab8d639b967d20c13d680d7
Miłosz Forystek <mforystek@9bits.pl>
2024-11-05 15:56:35 +0100

    MatchHistoryV1

---
+++ Models/Entities/GoalsMatch.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Entities
+{
+    public class GoalsMatch
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? PlayerId { get; set; }
+        public int? Goals { get; set; }
+    }
+}
---
+++ Models/Entities/MatchPoz.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ULO.UI.Models.Entities
+{
+    public class MatchPoz
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MatchId { get; set; }
+        public int Score1Value { get; set; }
+        public int Score2Value { get; set; }
+
+    }
+}
